210627dbab5457402e277dfe97adb546eead2fc2
Joy Du <dooyoe@gmail.com>
2014-08-20 22:32:13 +0800

    v4.1.5(WD)201408202231
    修改属性描述及浏览器支持数据

--- js/inner.js
+++ js/inner.js
@@ -810,7 +810,7 @@ if (!Global.isLocal && Global.name) {
 			'<ul>'+
 				'<li><a href="https://github.com/doyoe/css-handbook/issues" target="_blank" rel="external" class="external">提交问题</a></li>'+
 			'</ul>'+
-			'<p>说明：本文档兼容性测试基础环境为：windows系统；IE6-10, Firefox4-25, Chrome4-31, Win Safari5.1.7, Opera9.5-18</p>'+
+			'<p>说明：本文档兼容性测试基础环境为：windows系统；IE6.0+, Firefox4.0+, Chrome4.0+, Safari4.0+, Opera15.0+</p>'+
 		'</div>';
 
 		$('#title').append(testBrowser);
