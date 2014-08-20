44b2214591ad105fac2f196a6cadb34c71c09ec1
Ridge <ridge.liu@toursforfun.com>
2014-08-20 20:42:56 +0800

    更新readme， grunt补充注释

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,7 @@
 module.exports = function(grunt) {
 	"use strict";
 
+	// JSON格式文件读取
 	function readOptionalJSON(filepath) {
 		var data = {};
 		try {
@@ -8,14 +9,11 @@ module.exports = function(grunt) {
 		} catch (e) {}
 		return data;
 	}
-	var banner = "/* <%= pkg.name %> v<%= pkg.version %>\n * homepage: <%= pkg.homepage %>\n */\n";
 
-	// The concatenated file won"t pass onevar
-	// But our modules can
-
+	// grunt配置
 	grunt.initConfig({
 		pkg: readOptionalJSON("package.json"),
-		//js文档生成
+		// HTML错误检查
 		htmlhint: {
 			htmlfiles: {
 				src: ["./**/*.htm", "./**/*.html", "!./node_modules/**/*.html"]
@@ -23,11 +21,13 @@ module.exports = function(grunt) {
 		}
 	});
 
-	//文件变化监控插件
+	// HTML错误检查插件
 	grunt.loadNpmTasks("grunt-htmlhint");
 	// Default grunt
 	grunt.registerTask("default", ["htmlhint"]);
-	grunt.task.registerTask("htc", "build htc", function() {
-		//grunt.file.write("src/h5form.htc", grunt.file.read("src/banner.htc") + grunt.file.read("src/h5form.htc.js") + "\n</SCRIPT></PUBLIC:COMPONENT>");
+/*
+	grunt.task.registerTask("chm", "build chm", function() {
+		
 	});
+*/
 };
--- README.md
+++ README.md
@@ -14,3 +14,14 @@ CSS参考手册从v4.1.4开始，代码都托管到GitHub上。
 windows下安装`HTML Help Workshop`后，双击打开`css.hhp`，点击`编译HTML文件`按钮，即可完成编译
 
 修改chm目录：`contents.hhc`，修改chm索引：`index.hhk`
+
+## 代码错误检查
+
+为确保大家提交的代码的质量，提交代码之前请在项目根目录运行`grunt`命令检查你修改的HTML是否有语法错误
+
+**grunt安装方法**
+
+1. 安装[Node.js](http://nodejs.org/download/),安装后可能需要重启电脑
+1. 将安装源设置为中国地区，否则会很慢 `npm config set registry http://registry.cnpmjs.org/ --global`
+1. 命令行运行 `npm install -g grunt`
+1. 项目根目录运行`npm install`
