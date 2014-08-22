7e8d521b6cb30e7006cc08bb962bdabb3c498d63
Ridge <ridge.liu@toursforfun.com>
2014-08-22 10:58:23 +0800

    grunt自动生成chm索引

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,7 +63,7 @@ module.exports = function(grunt) {
 				} else {
 					delete obj.children;
 				}
-			} else if(obj.attribs){
+			} else if (obj.attribs) {
 				for (var i in obj.attribs) {
 					obj[i] = obj.attribs[i];
 				}
@@ -71,6 +71,37 @@ module.exports = function(grunt) {
 			}
 		}
 		queryHTML(obj, filter);
+		return obj;
+	}
+
+	function filterli(obj) {
+		queryHTML(obj, function(obj) {
+			if (obj.type == "tag") {
+				if (obj.name == "li") {
+					if (obj.children[0].href) {
+						obj.href = obj.children[0].href
+						obj.text = obj.children[0].text
+					}
+					if (obj.children[1]) {
+						if (obj.children[1].name == "ul") {
+							obj.children = obj.children[1].children;
+						}
+					} else {
+						delete obj.children;
+					}
+				}
+			}
+		});
+		return obj;
+	}
+
+	function filterProp(obj) {
+		queryHTML(obj, function(obj) {
+			delete obj.type;
+			delete obj.raw;
+			delete obj.name;
+			delete obj.data;
+		});
 		return obj;
 	}
 
@@ -102,11 +133,25 @@ module.exports = function(grunt) {
 		});
 		var parser = new htmlparser.Parser(handler);
 		parser.parseComplete(rawHtml);
-		var tree = filterTextNode(findFirstUl(findTree(handler.dom)));
-		tree = filterTextNode({children: tree.children});
+		var tree = filterProp(filterli(filterTextNode(findFirstUl(findTree(handler.dom)))));
 		if (tree) {
-			grunt.file.write("index.json", JSON.stringify(tree, 0, 4));
-			
+			tree = {
+				children: tree.children
+			};
+			var index = '<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN"><HTML><HEAD><meta name="GENERATOR" content="Microsoft&reg; HTML Help Workshop 4.1"><!-- Sitemap 1.0 --></HEAD><BODY><UL>'
+			queryHTML(tree, function(a) {
+				if (a.href) {
+					index += '\n<LI><OBJECT type="text/sitemap"><param name="Name" value="' + a.text + '"><param name="Local" value="' + a.href + '"></OBJECT>';
+				}
+			});
+			index += "\n</UL></BODY></HTML>";
+			grunt.file.write("index.hhk", index, {
+				// 如果没有指定 encoding ，则默认值同 grunt.file.defaultEncoding
+				// 如果 `contents` 是 Buffer ，则 encoding 无效
+				encoding: "GB2312"
+			});
+			//grunt.file.write("index.json", JSON.stringify(tree, 0, 4));
+
 			console.log(tree);
 		}
 	});
