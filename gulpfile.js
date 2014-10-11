dad5b50561948a081694f6fc7646c8e03ec2ed73
Ridge <ridge.liu@toursforfun.com>
2014-10-11 12:44:45 +0800

    去掉文件BOM头

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,20 @@ function readOptionalJSON(filepath) {
 	return data;
 }
 
+function parseHtml(rawHtml) {
+	var htmlparser = require("htmlparser"),
+		handler = new htmlparser.DefaultHandler(function(error, dom) {
+			if (error) {
+				//[...do something for errors...]
+			} else {
+				//[...parsing done, do something...]
+			}
+		}),
+		parser = new htmlparser.Parser(handler);
+	parser.parseComplete(rawHtml);
+	return handler.dom;
+}
+
 // HTML节点查找
 function queryHTML(obj, fn) {
 	if (obj) {
@@ -94,17 +108,221 @@ function recurse(rootdir, callback, subdir) {
 	});
 };
 
+// 生成连续空格
+function tab(num) {
+	var str = "";
+	for (var i = 0; i < num; i++) {
+		str += "\t";
+	}
+	return str;
+}
+
+// 比较两个版本号，v1>v2则返回值大于零 v1<v2则返回值大于0，若v1==v2则返回值等于0
+function compare(v1, v2) {
+	v1 = convert(v1);
+	v2 = convert(v2);
+	for (var diff = 0, i = 0;
+		(i < v1.length || i < v2.length) && diff === 0; i++) {
+		diff = parseNum(v1[i]) - parseNum(v2[i]);
+	}
+	return diff;
+}
+
+// 将版本号按小数点分割为数组
+function convert(ver) {
+	return ver.toString().split(".");
+}
+
+// 将字符串转为数字
+function parseNum(num) {
+	return parseInt(num) || 0;
+}
+
+// 使用caniuse.com数据自动生成兼容性图表
+function caniuseData(str, strIndent, strPropName, subName, index, html) {
+	strIndent = strIndent.match(/\t| {4}/g);
+	strIndent = strIndent ? tab(strIndent.length) : "";
+	// 缩进所用的数据
+	var indentData = {
+		thead: strIndent + tab(1),
+		tbody: strIndent + tab(1),
+		tr: strIndent + tab(2),
+		th: strIndent + tab(3),
+		td: strIndent + tab(3)
+	}
+
+	// 生成缩进
+	function indent(s, tag, tagName) {
+		return "\r\n" + (indentData[tagName] || strIndent) + tag;
+	}
+
+	var caniuse = require('caniuse-db/data'),
+		classFix = {
+			p: "experimentsupport",
+			a: "partsupport",
+			n: "unsupport",
+			y: "support"
+		},
+		propFix = {
+			"repeating-linear-gradient()": "css-repeating-gradients",
+			"repeating-radial-gradient()": "css-repeating-gradients",
+			"linear-gradient()": "css-gradients",
+			"radial-gradient()": "css-gradients",
+			"text-shadow": "textshadow",
+			"tab-size": "css3-tabsize",
+			"box-shadow": "boxshadow",
+			transform: "transforms2d",
+			"@media": "mediaqueries",
+			vmax: "viewport-units",
+			vmin: "viewport-units",
+			vw: "viewport-units",
+			vh: "viewport-units",
+			"rgba": "css3-colors",
+			"hsla": "css3-colors",
+			"hsl": "css3-colors",
+			"calc()": "calc",
+		},
+		regPropSub = /(-\w+)+$/,
+		regPropS = /s$/,
+		tabData = {},
+		rowNum = 0,
+		propName,
+		status,
+		thead,
+		tbody,
+		data,
+		i,
+		j,
+		k;
+
+	function getDate(prop) {
+		prop = propFix[prop] || prop;
+		data = caniuse.data[prop] || caniuse.data["css-" + prop];
+		if (!data && regPropSub.test(prop)) {
+			getDate(prop.replace(regPropSub, ""));
+		}
+		if (!data && !regPropS.test(prop)) {
+			getDate(prop + "s");
+		}
+		if (data) {
+			if (/^(vw|vh|vmin|(repeating-)?linear-gradient\(\))$/.test(propName)) {
+				data = JSON.parse(JSON.stringify(data).replace(/"a\b/g, "\"y"));
+			} else if (/^(vmax|(repeating-)?radial-gradient\(\))$/.test(propName)) {
+				data = JSON.parse(JSON.stringify(data).replace(/"a\b[^"]*/g, "\"n"));
+			}
+			propName = prop;
+		}
+	}
+
+	function getPrefix(bro, ver) {
+		bro = caniuse.agents[bro];
+		return (bro.prefix_exceptions || {})[ver] || bro.prefix;
+	}
+
+	getDate(strPropName);
+
+	if (!data) {
+		propName = queryHTML(parseHtml(html), function(obj) {
+			return obj.type == "tag" && obj.attribs && obj.attribs.id == "category";
+		});
+		if (propName) {
+			propName = propName.attribs.name;
+			getDate(propName);
+		}
+	}
+
+	if (!data) {
+		if (propName) {
+			console.log("caniuse数据中无此项：\t" + propName);
+		} else {
+			console.log("未指定caniuse查询项目。");
+		}
+	} else {
+		status = data.stats;
+		str = "<!-- compatible:" + strPropName + ' --><table class="g-data"><thead><tr>';
+		thead = "";
+		tbody = "";
+
+		for (i in status) {
+			thead += '<th><span class="browser-' + i + '">' + caniuse.agents[i].browser.replace(/\s+/g, "<br>") + '</span></th>';
+			tabData[i] = {};
+			for (j in status[i]) {
+				tbody = status[i][j];
+				if (!/\bu\b/i.test(tbody)) {
+					tbody = tbody.replace(/\bx\b/, function() {
+						return "-" + getPrefix(i, j) + "-";
+					});
+					if (propName === "transforms2d" && tbody === "y") {
+						tbody += getPrefix(i, j);
+					}
+					if (tabData[i][tbody]) {
+						tabData[i][tbody].push(j)
+					} else {
+						tabData[i][tbody] = [j];
+					}
+				}
+			}
+			for (j in tabData[i]) {
+				tbody = tabData[i][j].join(",").split(/\s*[,-]\s*/g).sort(compare);
+				if (tbody.length === 1) {
+					tabData[i][j] = tbody;
+				} else {
+					tabData[i][j] = [tbody[0], tbody[tbody.length - 1]];
+				}
+			}
+			if (tabData[i].y) {
+				tabData[i].y = [tabData[i].y[0]];
+			}
+			tbody = [];
+			for (j in tabData[i]) {
+				tbody.push({
+					className: ' class="' + classFix[j.substr(0, 1)] + '"',
+					prefix: /(-\w+-)/.test(j) ? (' <sup class="fix">' + RegExp.$1 + "</sup>") : "",
+					value: tabData[i][j],
+					type: j
+				});
+			}
+			tabData[i] = tbody.sort(function(a, b) {
+				return compare(a.value[0], b.value[0]);
+			});
+			rowNum = Math.max(rowNum, tbody.length);
+		}
+		for (i in tabData) {
+			tbody = rowNum - tabData[i].length + 1;
+			tabData[i][tabData[i].length - 1].rowspan = tbody > 1 ? ' rowspan="' + tbody + '"' : "";
+		}
+		tbody = "";
+		for (i = 0; i < rowNum; i++) {
+			tbody += "<tr>";
+			for (j in status) {
+				if (tabData[j][i]) {
+					tbody += "<td" + (tabData[j][i].rowspan || "") + tabData[j][i].className + ">" + tabData[j][i].value.join("-") + tabData[j][i].prefix + "</td>";
+				}
+			}
+			tbody += "</tr>";
+		}
+		str += thead + "</tr></thead><tbody>" + tbody + "</tbody></table><!-- compatible:end -->";
+		str = strIndent + str.replace(/(\s*<\/?(ul|div|section|tr|t\w{2,})(\s[^>]+)*>\s*)/ig, "\n$1\n").replace(/(<\/(li|h\d|th|td)>\s*)/ig, "$1\n").replace(/\n+(<[\/\!]?(\w+)?)/g, indent);
+	}
+	return str;
+}
+
 // html验证
 gulp.task("htm", function() {
-
 	console.log("正在检查所有html文件代码是否合法，请稍候~~~");
 
-	var htmlhint = require("gulp-htmlhint");
+	var replace = require('gulp-replace'),
+		htmlhint = require("gulp-htmlhint");
 	recurse(".", function(filepath, rootdir, subdir, filename) {
 		if (/\.html?$/.test(filename)) {
 			gulp.src(filepath)
+				.pipe(replace(/([\t ]*)<\!--\s*compatible\s*:\s*(\w+(-\w+)?)\s*-->[\s\S]*?<!--\s*compatible\s*:\s*end\s*-->/g, caniuseData))
+				.pipe(replace(/(\t|\n) {4,}/g, function(str, char) {
+					return char + tab(parseInt(str.length / 4));
+				}))
 				.pipe(htmlhint())
-				.pipe(htmlhint.reporter());
+				.pipe(htmlhint.reporter())
+				.pipe(gulp.dest(subdir || "."));
 		}
 	});
 });
@@ -112,18 +330,7 @@ gulp.task("htm", function() {
 //生成chm文件
 gulp.task("chm", function() {
 	console.log("正在生成工程文件");
-	var htmlparser = require("htmlparser"),
-		rawHtml = fs.readFileSync("index.htm"),
-		handler = new htmlparser.DefaultHandler(function(error, dom) {
-			if (error) {
-				//[...do something for errors...]
-			} else {
-				//[...parsing done, do something...]
-			}
-		}),
-		parser = new htmlparser.Parser(handler);
-	parser.parseComplete(rawHtml);
-	var tree = queryHTML(handler.dom, function(obj) {
+	var tree = queryHTML(parseHtml(fs.readFileSync("index.htm")), function(obj) {
 		return obj.type == "tag" && obj.attribs && obj.attribs.id == "dytree";
 	});
 
@@ -152,7 +359,7 @@ gulp.task("chm", function() {
 			files = {};
 
 		recurse(".", function(abspath, rootdir, subdir, filename) {
-			if (!/^(ZeroClipboard\.swf|\w+\.psd|selectivizr.js)$/.test(filename) && subdir && !/^images$/.test(subdir)) {
+			if (!/^(ZeroClipboard\.swf|prefixfree\.min\.js|\w+\.psd)$/.test(filename) && subdir && !/^images$/.test(subdir)) {
 				files[path.normalize(subdir + "/" + filename)] = true;
 			}
 		});
@@ -160,12 +367,10 @@ gulp.task("chm", function() {
 		forEachTree(tree, function(o) {
 			hhk += '<LI><OBJECT type="text/sitemap"><param name="Name" value="' + o.title + '"><param name="Local" value="' + o.href + '"></OBJECT>';
 			var filepath = path.normalize(o.href);
-			if (!files[filepath]) {
-				if (fs.existsSync(filepath)) {
-					files[filepath] = true;
-				} else {
-					console.log("发现死链接:\t" + o.href);
-				}
+			if (files[filepath] || fs.existsSync(filepath)) {
+				files[filepath] = "ok";
+			} else {
+				console.log("发现死链接(文件不存在):\t" + o.href);
 			}
 			hhc += '<LI><OBJECT type="text/sitemap"><param name="Name" value="' + o.title + '"><param name="Local" value="' + o.href + '"><param name="ImageNumber" value="' + (o.children ? 1 : (/\//.test(o.href) ? 11 : 15)) + '"></OBJECT>'
 		});
@@ -175,7 +380,13 @@ gulp.task("chm", function() {
 		hhc += "</BODY></HTML>";
 
 		for (var i in files) {
-			hhp += i + "\n";
+			if (/\.html?$/.test(i)) {
+				if (files[i] !== "ok") {
+					console.log("发现死文件(没有链接指向此文件):\t" + i);
+				}
+			} else {
+				hhp += i + "\n";
+			}
 		}
 
 		var iconv = require('iconv-lite');
