3a6794a9ec2f52c0bc8aaf139933ef58df46dd2e
Joy Du <dooyoe@gmail.com>
2016-12-07 12:32:35 +0800

    添加 -webkit-overflow-scrolling 属性参考

--- js/inner.js
+++ js/inner.js
@@ -470,7 +470,8 @@ if (!Global.isLocal && Global.name) {
 					'text-stroke-width' : [],
 					'text-stroke-color' : [],
 					'tap-highlight-color' : [],
-					'user-drag' : []
+					'user-drag' : [],
+					'-webkit-overflow-scrolling' : []
 				}
 			},
 
